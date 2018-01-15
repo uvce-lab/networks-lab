2c46d919c934dedcbcbff9aae20c89f855954430
Saurav Sahu <sauravsahu.12@live.com>
2018-01-15 13:04:49 +0530

    Refactor RSA.java.

---
+++ RSA.java
@@ -0,0 +1,78 @@
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class RSA {
+    private static BigInteger p, q, factor, totient, publicKey, privateKey;
+    private static Map<BigInteger, Character> map = new HashMap<>();
+
+    static void setup() {
+        p = new BigInteger("61");
+        q = new BigInteger("53");
+        factor = p.multiply(q);
+        totient = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+
+        publicKey = BigInteger.valueOf(17);
+        privateKey = BigInteger.valueOf(2753);
+    }
+
+    static void _status() {
+        System.out.println("\np = " + p + "\nq = " + q + " \nfactor = " + factor + "\ntotient = " + totient);
+        System.out.println("\npublicKey = " + publicKey + "\nprivateKey = " + privateKey);
+    }
+
+    private static void map_setup(Map<BigInteger, Character> map) {
+        for (int i = 32; i < 127; i++)
+            map.put(BigInteger.valueOf(i), (char) i);
+    }
+
+    public static BigInteger[] encrypt(String message) {
+        BigInteger[] array = new BigInteger[message.length()];
+        map_setup(map);
+
+        for (int i = 0; i < message.length(); i++) {
+            BigInteger tmp = BigInteger.valueOf((int) message.charAt(i));
+            tmp = tmp.modPow(publicKey, factor);
+            array[i] = tmp;
+        }
+
+        System.out.println("\nThe Cipher Text :");
+        for (int i = 0; i < array.length; i++)
+            System.out.print(array[i] + " ");
+        System.out.println();
+
+        return array;
+    }
+
+    public static void decrypt(BigInteger[] array) {
+        Character[] arr = new Character[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            BigInteger tmp = array[i].modPow(privateKey, factor);
+            arr[i] = map.get(tmp);
+        }
+
+        System.out.println("\nThe Recovered Plain Text :");
+        for (int i = 0; i < array.length; i++)
+            System.out.print(arr[i]);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner bt = new Scanner(System.in);
+        setup();
+        _status();
+        
+        System.out.print("Give the message : ");
+        String message = bt.nextLine();
+        BigInteger[] array = new BigInteger[message.length()];
+        array = encrypt(message);
+
+        System.out.println("\n\nDo you want to decrypt the message (1/0)");
+        int choice = bt.nextInt();
+        if (choice == 1)
+            decrypt(array);
+        bt.close();
+    }
+}
